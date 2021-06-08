9e9437b983ba757268c70039078520ad1b730b8b
gillyhuga <gilly.huga@yahoo.co.id>
2021-06-08 22:59:29 +0700

    add Auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        if(Auth::check())
+        {
+            redirect()->route('dashboard.index');
+        }
+    }
+
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => [
+                'required',
+            ],
+            'password' => [
+                'required',
+            ],
+        ]);
+
+        if($validator->fails())
+        {
+            $errors = $validator->errors();
+
+            return response()->json(['success' => false, 'message' => $errors]);
+        }
+
+        $token = Auth::guard('api')->attempt(['email' => $request->email, 'password' => $request->password, 'role' => 'user']);
+        if(!$token)
+        {
+            return response()->json(['success' => false, 'message' => ['login' => ['Email dan Password Salah']]]);
+        }
+
+        return $this->createToken($token);
+    }
+
+    public function logout()
+    {
+        Auth::logout(); // menghapus session yang aktif
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+
+    protected function createToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            'user' => Auth::user()
+        ]);
+    }
+
+    public function refresh() {
+        return $this->createNewToken(Auth::refresh());
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+            'status' => 'error',
+            'success' => false,
+            'error' =>
+            $validator->errors()->toArray()
+            ], 400);
+        }
+        
+        $user = User::create([
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'role' => 'user',
+        ]);
+         
+        return response()->json([
+            'message' => 'User has been created!',
+            'user' => $user
+            ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -17,9 +18,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -37,7 +38,11 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }   
 }
